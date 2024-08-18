32190e5b9e24bf2bbd3011f05747fdfc6b4ded4f
Kyle <kyle201817146@gmail.com>
2024-08-18 21:30:06 +0800

    Add compatibility for old script

---
+++ override_apple_intelligence.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\033[31mThis script has been moved to https://github.com/Kyle-Ye/XcodeLLMEligible/blob/release/0.1/override_apple_intelligence.sh\033[0m"
+echo -e "\033[31mRecommend to use the new script on https://github.com/Kyle-Ye/XcodeLLMEligible/blob/release/0.2/Scripts/override.sh\033[0m"
+echo -e "\033[31mFor more detail, see https://github.com/Kyle-Ye/XcodeLLMEligible/tree/release/0.2\033[0m"
